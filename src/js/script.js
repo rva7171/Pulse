ee352c3ff8ca0d50cf7ccfb2c4fed19e30f1a233
rva7171 <rechmed@yandex.ru>
2021-01-07 18:59:56 +0300

    добавлены все анимации

--- src/js/script.js
+++ src/js/script.js
@@ -118,4 +118,22 @@ $(document).ready(function () {
       });
       return false;
    });
+
+   // Скролл в начало по иконке pageup
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $(".pageup").fadeIn();
+      } else {
+         $(".pageup").fadeOut();
+      }
+   });
+
+   $("a[href^='#']").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
+
+   new WOW().init();
 });
