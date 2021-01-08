8dc838979ae4442797c1803cf7ef900748602516
rva7171 <rechmed@yandex.ru>
2021-01-08 22:22:36 +0300

    все оптимизированно с добавлением в dist

---
+++ dist/js/script.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+   $(".carousel__inner").slick({
+      speed: 1200,
+      adaptiveHeight: true,
+      prevArrow:
+         '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      nextArrow:
+         '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      responsive: [
+         {
+            breakpoint: 992,
+            settings: {
+               dots: true,
+               arrows: false,
+            },
+         },
+      ],
+   });
+
+   $("ul.catalog__tabs").on(
+      "click",
+      "li:not(.catalog__tab_active)",
+      function () {
+         $(this)
+            .addClass("catalog__tab_active")
+            .siblings()
+            .removeClass("catalog__tab_active")
+            .closest("div.container")
+            .find("div.catalog__content")
+            .removeClass("catalog__content_active")
+            .eq($(this).index())
+            .addClass("catalog__content_active");
+      }
+   );
+
+   function toggleSlide(item) {
+      $(item).each(function (i) {
+         $(this).on("click", function (e) {
+            e.preventDefault();
+            $(".catalog-item__content")
+               .eq(i)
+               .toggleClass("catalog-item__content_active");
+            $(".catalog-item__list")
+               .eq(i)
+               .toggleClass("catalog-item__list_active");
+         });
+      });
+   }
+
+   toggleSlide(".catalog-item__link");
+   toggleSlide(".catalog-item__back");
+
+   // Модальные окна
+
+   $("[data-modal=consultation]").on("click", function () {
+      $(".overlay, #consultation").fadeIn("slow"); // появление формы
+   });
+   //сокрытие форм по крестику
+   $(".modal__close").on("click", function () {
+      $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+   });
+   //на кнопках покупки в каталоге
+   $(".button_mini").each(function (i) {
+      $(this).on("click", function () {
+         $("#order .modal__descr").text(
+            $(".catalog-item__subtitle").eq(i).text()
+         );
+         $(".overlay, #order").fadeIn("slow");
+      });
+   });
+   //валидация форм
+   function validateForms(form) {
+      $(form).validate({
+         rules: {
+            name: {
+               required: true,
+               minlength: 2,
+            },
+            phone: "required",
+            email: {
+               required: true,
+               email: true,
+            },
+         },
+         messages: {
+            name: {
+               required: "Введите свое имя",
+               minlength: jQuery.validator.format("Введите {0} символа!"),
+            },
+            phone: "Введите свой номер телефона",
+            email: {
+               required: "Введите свою почту",
+               email: "Неправильно введен адрес почты",
+            },
+         },
+      });
+   }
+
+   validateForms("#consultation-form");
+   validateForms("#consultation form");
+   validateForms("#order form");
+
+   $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+   // форма подтверждения и отправка почты
+   $("form").submit(function (e) {
+      e.preventDefault(); //отменяем стандарт поведение броузера, будет без перезагрузки
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize(),
+      }).done(function () {
+         $(this).find("input").val("");
+         $("#consultation, #order").fadeOut();
+         $(".overlay, #thanks").fadeIn("slow"); //показываем форму благодарности
+
+         $("form").trigger("reset");
+      });
+      return false;
+   });
+
+   // Скролл в начало по иконке pageup
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $(".pageup").fadeIn();
+      } else {
+         $(".pageup").fadeOut();
+      }
+   });
+
+   $("a[href^='#up']").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
+
+   new WOW().init();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,78 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass");
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
+const htmlmin = require("gulp-htmlmin");
+const imagemin = require("gulp-imagemin");
 
-gulp.task('server', function() {
+// запуск сервера
+gulp.task("server", function () {
+   browserSync({
+      server: {
+         baseDir: "dist", //запуск оптимизированного сервера
+      },
+   });
 
-    browserSync({
-        server: {
-            baseDir: "src"
-        }
-    });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+   gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+// обработка и формирование файлов
+gulp.task("styles", function () {
+   return gulp
+      .src("src/sass/**/*.+(scss|sass)")
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      .pipe(rename({ suffix: ".min", prefix: "" }))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      .pipe(gulp.dest("dist/css")) // помещаем обработанный файл в dist
+      .pipe(browserSync.stream());
+});
+// отслеживаем изменения в файлах
+gulp.task("watch", function () {
+   gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+   gulp.watch("src/*.html").on("change", gulp.parallel("html"));
+});
+// обработка html и помещение в dist
+gulp.task("html", function () {
+   return gulp
+      .src("src/*.html")
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest("dist/"));
+});
+// перемешение скриптов в dist без изменений
+gulp.task("scripts", function () {
+   return gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js"));
+});
+// перемешение шрифтов в dist без изменений
+gulp.task("fonts", function () {
+   return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
+// перемешение иконок в dist без изменений
+gulp.task("icons", function () {
+   return gulp.src("src/icons/**/*").pipe(gulp.dest("dist/icons"));
+});
+// перемешение mailer в dist без изменений
+gulp.task("mailer", function () {
+   return gulp.src("src/mailer/**/*").pipe(gulp.dest("dist/mailer"));
+});
+// перемешение картинок в dist со сжатием
+gulp.task("images", function () {
+   return gulp.src("src/img/**/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+// перезапуск styles при изменениях
+gulp.task(
+   "default",
+   gulp.parallel(
+      "watch",
+      "server",
+      "styles",
+      "scripts",
+      "fonts",
+      "icons",
+      "images",
+      "mailer",
+      "html"
+   )
+);
